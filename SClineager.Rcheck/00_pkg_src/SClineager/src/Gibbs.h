6fd4ad9b997a8a7a73e118bc842ba5957972382a
inmybrain <seongohpark6@gmail.com>
2020-03-11 07:46:25 +0900

    add 'keep_genes' argument in 'run_sclineager'

---
+++ SClineager.Rcheck/00_pkg_src/SClineager/src/Gibbs.h
@@ -0,0 +1,26 @@
+//------- Source from SClineager_Gibbs_ver2.cpp: do not edit by hand
+#ifndef Gibbs
+#define Gibbs
+
+double getPostLike(arma::mat k_mat,
+                   arma::mat transform_mat,
+                   arma::mat genotype_mat,
+                   arma::uvec is_high_cover,
+                   arma::vec mu,
+                   arma::mat sigma,
+                   arma::mat psi,
+                   int dfreedom,
+                   bool logd)
+;
+Rcpp::List sclineager_gibbs(
+    arma::mat psi,
+    arma::mat k_mat,
+    arma::mat transform_mat,
+    arma::vec mu,
+    int dfreedom,
+    arma::mat sigma,
+    int max_iter = 100,
+    bool save = false
+)
+;
+#endif
